ba59934ce97ff4dda7dafd835d8c1b40de4bf7c4
Kenneth! <121078488+Kookiejarz@users.noreply.github.com>
2024-05-19 20:50:25 +0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,7 +23,7 @@ export default function Header() {
         <div>
           <a href="https://github.com/Kookiejarz/Monisite" target="_blank">
             <Text size="xl" span>
-              🔭\t
+              🔭🌍
             </Text>
             <Text
               size="xl"
