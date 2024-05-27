d539ce1b5b77a5a1e57f91d726e5c3a271c0dd16
Kenneth! <121078488+Kookiejarz@users.noreply.github.com>
2024-05-27 19:53:37 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -70,7 +70,7 @@ const workerConfig = {
       // `method` should be `TCP_PING` for tcp monitors
       method: 'TCP_PING',
       // `target` should be `host:port` for tcp monitors
-      target: '175.41.157.111:444',
+      target: '175.41.157.111:446',
       tooltip: 'Singapore',
     //statusPageLink: 'https://dl.btlcraft.top',
       timeout: 5000,
